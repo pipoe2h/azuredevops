2b52eb8494b196527b00e9eec9330a6f2779cdc1
Jose Gomez <pipoe2h@gmail.com>
2019-05-12 22:54:32 +0100

    Update calm.py

--- calm.py
+++ calm.py
@@ -3,6 +3,19 @@ import json
 import os
 from base64 import b64encode
 import sys
+import requests
+requests.packages.urllib3.disable_warnings()
+
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 class PcManager():
 
@@ -23,9 +36,7 @@ class PcManager():
         self.response_file = response_file
 
     # Create a REST client session.
-    def rest_call(self):
-        urllib3.disable_warnings()
-        
+    def rest_call(self):        
         base_url = 'https://%s:9440/api/nutanix/v3/%s' % (
             self.ip_addr, self.sub_url)
         if self.body and self.content_type == "application/json":
